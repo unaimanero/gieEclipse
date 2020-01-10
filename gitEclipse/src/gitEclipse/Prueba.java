cfdfc93346a20c03cdf1b488570789c8fbb940e9
Unai <unai.manero@nter.es>
2020-01-10 08:47:38 +0100

    Commit nº 1

---
+++ gitEclipse/src/gitEclipse/Prueba.java
@@ -0,0 +1,13 @@
+package gitEclipse;
+
+public class Prueba {
+  public static void main(String[] args) 
+  {
+     int n = 2;
+     if(n%2 ==0) 
+     {
+       System.out.println("Todo correcto");
+     }
+      
+  }
+}
